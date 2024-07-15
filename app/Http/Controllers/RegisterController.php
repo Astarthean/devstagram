d3cc03d10db10854f0c296f7aceaf6f060106a85
Ana <asakalieu@gmail.com>
2024-07-15 13:12:31 +0200

    Registro de usuarios en DB  y Login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -16,12 +19,35 @@ class RegisterController extends Controller
         // dd($request);
         // dd($request->get('name'));
 
+        //Modificar el request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         //Validación
         $request->validate([
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|email|unique:users|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
             ]);
+
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            //Autenticar un usuario
+            // auth()->attempt([
+            //     'email' => $request->email,
+            //     'password' => $request->password
+            // ]);
+
+            //Otra forma de autenticar
+            auth()->attempt($request->only('email', 'password'));
+
+            //Redireccionar al usuario
+            return redirect()->route('posts.index');
+
     }
 }
